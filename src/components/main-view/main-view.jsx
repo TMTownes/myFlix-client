8c23cef956d9f8c812e21e6e45227b0ee9ddb395
Tabitha_Townes <46570035+TMTownes@users.noreply.github.com>
2023-09-26 23:41:35 -0700

    new Signup View

--- src/components/main-view/main-view.jsx
+++ src/components/main-view/main-view.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from "react";
 import { MovieCard } from "../movie-card/movie-card";
 import { MovieView } from "../movie-view/movie-view";
 import { LoginView } from "../login-view/login-view";
+import { SignupView } from "../signup-view/signup-view";
 
 export const MainView = () => {
   const storedUser = JSON.parse(localStorage.getItem("user"));
@@ -29,12 +30,20 @@ export const MainView = () => {
   }, [token]);
 
   if(!user) {
-    return (<LoginView 
+    return (
+    <>
+    Users Login
+    <LoginView 
       onLoggedIn={(user, token) => {
         setUser(user);
       setToken(token);
       }}
       />
+      <br/>
+
+      Create an Account!
+      <SignupView />
+    </>
     ); 
   } 
 
