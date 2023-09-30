8723e1ed9d0e9ccdb38516ec4196b7ea479764cc
Tabitha_Townes <46570035+TMTownes@users.noreply.github.com>
2023-09-29 18:23:30 -0700

    added Movie Card component

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,6 @@
 import { createRoot } from "react-dom/client";
 import { MainView } from "./components/main-view/main-view";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 
 //Import statement: indicates need to bundle `./index.scss`
 import "./index.scss";
