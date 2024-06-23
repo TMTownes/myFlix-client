5c158492e4950b89ad7217acf0a584d44277f262
Tabitha_Townes <46570035+TMTownes@users.noreply.github.com>
2024-06-23 13:10:38 -0500

    Updated SearchBox

--- src/components/SearchBox/searchBox.jsx
+++ src/components/SearchBox/searchBox.jsx
@@ -4,23 +4,25 @@ import PropTypes from "prop-types";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./searchBox.scss";
 
-export const SearchBox = ({ query, handleSearch }) => {
-
+export const SearchBox = ({handleSearch, query}) => {
 
   return (
     <Row className="searchBox">
       <Form.Control
         type="text"
         value={query}
-        onChange={handleSearch}
-        placeholder="Search"
-        className="row d-flex align-items-auto mt4 mb4 mb4"
+        onChange={(e) => handleSearch(e)}
+        placeholder="Search..."
+        className="row d-flex align-items-auto mt-6 mb-6"
        />
-
     </Row>
   );
 };
+
 SearchBox.propTypes = {
+  // movie: PropTypes.object
   handleSearch: PropTypes.func,
-  query: PropTypes.string
+  query: PropTypes.string,
+  // setQuery: PropTypes.func, 
+  // onSearch: PropTypes.func
 };
