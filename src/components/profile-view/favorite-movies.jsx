bc840547d4e74a75333783f359c0b4b801dee6bd
Tabitha_Townes <46570035+TMTownes@users.noreply.github.com>
2024-07-29 19:48:51 -0500

    Updated css for login/profile/main views

--- src/components/profile-view/favorite-movies.jsx
+++ src/components/profile-view/favorite-movies.jsx
@@ -2,29 +2,28 @@ import React from "react";
 import PropTypes from "prop-types";
 import Row from "react-bootstrap/Row";
 import Col from "react-bootstrap/Col";
-import Figure from "react-bootstrap/Figure";
 import { MovieCard } from "../movie-card/movie-card";
 import "./profile-view";
+import { Card } from "react-bootstrap";
 
 export const FavoriteMovies = ({favoriteMovies}) => {
 
   
   return (
-    <Col className="mb-5">
+    <Col className="mb-4">
       <Row className="row">
-        <Figure>
+        <Card className="d-flex flex-row flex-nowrap overflow-auto m-3">
         {favoriteMovies.map((movie) => (
           <Col xs={12} md={10} lg={6} key={movie.id} className="fav-movies">
            
             <MovieCard
             key={movie.id}
-            //what are we doing with this? Just need to confirm its a fav
             isFavorite={true}
             movie={movie}
             />
           </Col> 
         ))}
-        </Figure>
+        </Card>
       </Row>
     </Col>
   );
